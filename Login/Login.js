212d27d156af1ba91c868581d526bfd77c198d51
Vicente Woinilowicz <139511901+woinivincent@users.noreply.github.com>
2024-02-06 22:43:12 -0300

    se agregron las funcionalidades y los formularios

---
+++ Login/Login.js
@@ -0,0 +1,50 @@
+const hidePasswordIcon = document.getElementById('hide-password');
+const showPasswordIcon = document.getElementById('show-password');
+const passwordInput = document.getElementById('password');
+
+hidePasswordIcon.addEventListener('click', function() {
+    passwordInput.setAttribute('type', 'text');
+    hidePasswordIcon.style.display = 'none';
+    showPasswordIcon.style.display = 'inline';
+});
+
+showPasswordIcon.addEventListener('click', function() {
+    passwordInput.setAttribute('type', 'password');
+    showPasswordIcon.style.display = 'none';
+    hidePasswordIcon.style.display = 'inline';
+});
+    
+
+
+
+document.querySelector('.login-btn').addEventListener('click', function (event) {
+    event.preventDefault();
+    validateLoginForm();
+});
+
+function validateLoginForm() {
+    const email = document.getElementById('mobile-email').value;
+    const password = document.getElementById('password').value;
+    // Validación 
+    if (email.trim() === "") {
+        alert("Por favor, ingrese su correo electrónico");
+        return false;
+    }
+
+    // Validación del formato de correo electrónico 
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        alert("Ingrese un correo electrónico válido");
+        return false;
+    }
+
+    // Validación de la contraseña
+    if (password.trim() === "") {
+        alert("Por favor, ingrese su contraseña");
+        return false;
+    }
+
+    // Si la validación es exitosa, redirige a la página de inicio
+    window.location.href = "/index.html";
+
+}
---
+++ Register/app.js
@@ -0,0 +1,42 @@
+
+
+
+    document.getElementById('continueBtn').addEventListener('click', function () {
+        validateForm();
+    });
+
+    function validateForm() {
+        var name = document.getElementById('name').value;
+        var email = document.getElementById('mobile-email').value;
+        var password = document.getElementById('password').value;
+        var rePassword = document.getElementById('re-password').value;
+
+        // Validación  de todos los formularios
+        if (name === "" || email === "" || password === "" || rePassword === "") {
+            alert("Todos los campos son obligatorios");
+            return false;
+        }
+
+        // Validación del formato de correo electrónico
+        var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            alert("Ingrese un correo electrónico válido");
+            return false;
+        }
+
+        if (password.length < 6) {
+            alert("La contraseña debe tener al menos 6 caracteres");
+            return false;
+        }
+
+        if (password !== rePassword) {
+            alert("Las contraseñas no coinciden");
+            return false;
+        }
+
+        // Si todos los campos son válidos, enviar formulario y redirigir a la home
+     
+       
+        window.location.href = "/index.html";
+    }
+
