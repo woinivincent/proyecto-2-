9891925ed0f0fc1deffc30e3b113ba3bb27b06d8
Daniel777 <120663021+DanielSw777@users.noreply.github.com>
2024-02-12 18:50:39 -0300

    feat: Pagina productos y carrito

---
+++ Products/products.js
@@ -0,0 +1,16 @@
+/*
+ <section class="container-products">
+    <template id="card-product">
+        <div class="product-card">
+            <img src="ruta/a/la/imagen-del-producto.jpg"
+                alt="Nombre del producto">
+            <h3 class="product-name">Nombre del producto</h3>
+            <p class="product-price">$XX.XX</p>
+            <button class="add-to-cart-btn">Agregar al
+                carrito</button>
+        </div>
+    </template>
+</section>
+*/
+
+const 
